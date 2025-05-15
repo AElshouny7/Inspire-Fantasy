53ce1b50cb232b9209159be2ae7266bb16d299aa
Elshouny <elshouny7@gmail.com>
2025-05-16 00:09:22 +0300

    using data for payload

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,10 +2,16 @@ const BACKEND_URL =
   "https://script.google.com/macros/s/AKfycbwy6U1JzUiukosZWpUl6eVathT4G6zqbMo9WZBbHyp-kZIZJdgFifNf-8mXX7_J0QXlFA/exec";
 
 export async function callBackend(action, payload = {}) {
+  const formData = new URLSearchParams();
+  formData.append("action", action);
+  formData.append("data", JSON.stringify(payload)); // only one param `data`
+
   const res = await fetch(BACKEND_URL, {
     method: "POST",
-    body: JSON.stringify({ action, ...payload }),
-    headers: { "Content-Type": "application/json" },
+    body: formData, // NOT JSON.stringify
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
   });
 
   return res.json();
